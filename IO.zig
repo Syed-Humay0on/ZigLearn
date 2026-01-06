4b91b8416d1ef02b238999b3d0e5160b47336160
Proto <Humayoon04syed@gmail.com>
2026-01-07 00:30:57 +0500

    dereferencing uneven pointer

--- IO.zig
+++ IO.zig
@@ -4,7 +4,6 @@ const Io = std.Io; // Note the capital I
 pub fn main() !void {
     // In the new world, we often use 'any_writer' 
     // or specific IO types that implement the new interface.
-    const stdout_file = std.posix.STDOUT_FILENO;
-    const stdout = std.fs.File{ .handle = stdout_file};
+    const stdout = std.fs.File{ .handle = std.posix.STDOUT_FILENO };
     try stdout.writeAll("Hello from the new std.IO world\n");
 }
--- pointers.zig
+++ pointers.zig
@@ -4,17 +4,18 @@ const MathError = error{NotEven};
 
 pub fn main() void {
     var n: u8 = 254;
-    const m: *u8 = getEvenPointer(&n) catch | err |{
+    const m: *u8 = getEvenPointer(&n) catch |err| {
         std.debug.print("Failed because {}\n", .{err});
-            return;
-};
+        return;
+    };
     const o: u8 = m.*;
 
-    std.debug.print("Success: Value is: {}\n", .{ o });
+    std.debug.print("Success: Value is: {}\n", .{o});
 }
+
 fn getEvenPointer(ptr: *u8) MathError!*u8 {
     if (ptr.* % 2 != 0) {
-return MathError.NotEven;
+        return MathError.NotEven;
     }
     return ptr;
 }
