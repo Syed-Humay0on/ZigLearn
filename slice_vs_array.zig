b4dfdc7552df73f24071ed397a8f24b0d9a78e70
Proto <Humayoon04syed@gmail.com>
2026-01-16 23:08:24 +0500

    Zig: [array, slice and pointers]

---
+++ slice_vs_array.zig
@@ -0,0 +1,17 @@
+const std = @import("std");
+
+pub fn main() void {
+    var array = [5]u8{ 10, 20, 30, 40, 50 };
+
+    // This slice starts at index 2 (the number 30)
+    const slice = array[2..4];
+
+    const addr_from_array = &array[0]; // Points to 10
+    const addr_from_slice = &slice[0]; // Points to 30!}
+    const elem_from_array = array[0]; // Points to 10
+    const elem_from_slice = slice[0]; // Points to 30!}
+                                                  //
+    std.debug.print("Addr of 1st elem of arr: {}\nAddr of 1st elem of slice: {}\n", .{addr_from_array, addr_from_slice});
+    std.debug.print("1st Elem of arr: {}\n1st Elem of slice: {}\n", .{elem_from_array, elem_from_slice});
+
+}
